8648279fa0c616bae4264228dcf188fd02d5e988
elcode13 <elkhanjafarov07@gmail.com>
2021-04-30 11:13:18 +0400

    Initial commit

---
+++ src/main/java/ColectionHW.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColectionHW {
+
+    public static void main(String[] args) {
+        Contact contact = new Contact();
+        contact.add("555930883","Amina");
+        contact.add("555930884","Elkhan");
+        contact.add("555930885","Ramin");
+        contact.add("555930886","Lenura");
+        contact.get("Amina");
+        repetitionwords();
+    }
+
+    private static void repetitionwords() {
+        String[] words = {"Elkhan","Amina","Ramin","Elkhan","Lenura","Ramin","Venera","Atrafil","Lenura","Fatima"};
+        List<String> word = new ArrayList<>();
+        for(int i=0; i<words.length; i++) {
+            for (int j=i+1; j<words.length; j++) {
+                if(words[i].equals(words[j])) {
+                    word.add(words[j]);
+                }
+            }
+        }
+        for (String s :
+                word) {
+            System.out.println(s);
+        }
+    }
+}
+
---
+++ src/main/java/Contact.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Contact {
+    private Map<String, String> phonebook;
+    Contact() {
+        phonebook = new HashMap<>();
+    }
+    public void add(String number, String surname) {
+        phonebook.put(number, surname);
+    }
+    public void get(String surname){
+        if(phonebook.containsValue(surname)) {
+            Set<Map.Entry<String, String>> set = phonebook.entrySet();
+            for (Map.Entry<String, String> temp : set) {
+                if(temp.getValue().equals(surname)) {
+                    System.out.println(temp.getValue() + " : " + temp.getKey());
+                }
+            }
+        } else {
+            System.out.println("Such a name is not on the list.");
+        }
+    }
+
+}
+
